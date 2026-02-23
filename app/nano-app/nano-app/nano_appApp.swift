18dec1e28180f0e7a7a255fb0ede02178b616ba4
Jack Graziani <jackandrewgraziani@gmail.com>
2026-02-22 20:10:27 -0500

    front end draft

---
+++ app/nano-app/nano-app/nano_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nano_appApp.swift
+//  nano-app
+//
+//  Created by Jack Graziani on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct nano_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
